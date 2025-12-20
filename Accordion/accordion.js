c01d35859b1385bd6c4784bcf3966ce258dded93
Abdulhafiz redwan <hafizredwan10@gmail.com>
2025-12-20 00:09:50 +0000

    3rd simple project

---
+++ Accordion/accordion.js
@@ -0,0 +1,10 @@
+let question = document.getElementsByClassName('question');
+const accordion = document.getElementsByClassName('content-container');
+
+
+function questionBtn(){
+    answer.style.overflow = 'visible';
+   
+}
+
+question.add
